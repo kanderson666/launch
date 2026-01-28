2e8c263410c9873d7f1c6e60d95bab69cf8bbd35
kanderson666 <anderson.kj66@gmail.com>
2026-01-28 01:13:49 +0000

    add py119 palindromes

---
+++ py110/lesson_3/21.py
@@ -0,0 +1,180 @@
+import random
+from os import system
+FACES = 'JQKA'
+JQK_VALUE = 10
+NUM_SUITS = 4
+
+DECK = 0
+DEALER = 1
+DEALER_HIDDEN = 2
+PLAYER = 3
+HIDDEN_CARD = '?'
+
+HAND_SUM = 0
+FIRST_CARD = 1
+LIMIT = 21
+DEALER_LIMIT = LIMIT - 4
+
+YES_NO = 'yesno'
+FIRST_TO = 3
+
+def play():
+    cards = initialize_game()
+    display_hands(cards, DEALER_HIDDEN)
+
+    player_turn(cards)
+    if bust(cards, PLAYER):
+        display_hands(cards, DEALER)
+        print("\nPlayer bust! Dealer wins")
+        return DEALER
+
+    comp_turn(cards)
+    if bust(cards, DEALER):
+        display_hands(cards, DEALER)
+        print("\nDealer bust! Player wins")
+        return PLAYER
+
+    return determine_winner(cards)
+
+def initialize_game():
+    cards = [[], [0], [0], [0]]
+    cards[DECK] = fresh_deck()
+
+    deal(cards)
+
+    count_cards(cards, DEALER)
+    count_cards(cards, DEALER_HIDDEN)
+    count_cards(cards, PLAYER)
+
+    return cards
+
+def fresh_deck():
+    deck = [str(num) for num in range(2, 11)] * NUM_SUITS + list(FACES) * NUM_SUITS
+    random.shuffle(deck)
+    return deck
+
+def deal(cards):
+    hit(cards, PLAYER)
+    hit(cards, DEALER)
+    hit(cards, PLAYER)
+    hit(cards, DEALER)
+
+    cards[DEALER_HIDDEN] = [0, cards[DEALER][FIRST_CARD], HIDDEN_CARD]
+
+def hit(cards, player):
+    cards[player].append(cards[DECK].pop())
+
+def count_cards(cards, player):
+    total = 0
+    num_aces = 0
+    for card in cards[player][FIRST_CARD:]:
+        if card == HIDDEN_CARD:
+            break
+        if card not in FACES:
+            total += int(card)
+        elif card == 'A':
+            num_aces += 1
+        else:
+            total += JQK_VALUE
+    total = convert_aces(total, num_aces)
+    cards[player][HAND_SUM] = total
+
+def convert_aces(total, num_aces):
+    highest_value = 11 * num_aces  # if 1 ace, max value 11. If 2 aces, max value 22, etc
+
+    # repeat checks until all aces are of value 1
+    while highest_value > num_aces:
+        if total + highest_value <= LIMIT:
+            break
+        highest_value -= 10     # change 1 ace to value of 1
+    return total + highest_value
+
+def player_turn(cards):
+    while not bust(cards, PLAYER):
+        response = yes_or_no("\nHit? [y/n]: ")
+        if response[0] == 'n':
+            break
+
+        hit(cards, PLAYER)
+        count_cards(cards, PLAYER)
+        display_hands(cards, DEALER_HIDDEN)
+
+def comp_turn(cards):
+    display_hands(cards, DEALER)
+    input("Press Enter to continue ")
+
+    while cards[DEALER][HAND_SUM] < DEALER_LIMIT:
+        hit(cards, DEALER)
+        count_cards(cards, DEALER)
+        display_hands(cards, DEALER)
+        input("Press Enter to continue ")
+
+def bust(cards, player):
+    return cards[player][HAND_SUM] > LIMIT
+
+def yes_or_no(question):
+    while True:
+        response = input(question).casefold().strip()
+        if response == '':
+            pass
+        elif response in YES_NO and (response[0] == 'y' or response[0] == 'n'):
+            return response
+
+        print("Please enter valid response.")
+
+def display_hands(cards, dealer):
+    system("clear")
+    print("Dealer:")
+    print(f"[{']['.join(cards[dealer][FIRST_CARD:])}] Total: {cards[dealer][HAND_SUM]}")
+    print("\nPlayer:")
+    print(f"[{']['.join(cards[PLAYER][FIRST_CARD:])}] Total: {cards[PLAYER][HAND_SUM]}")
+
+def determine_winner(cards):
+    display_hands(cards, DEALER)
+
+    if cards[PLAYER][HAND_SUM] > cards[DEALER][HAND_SUM]:
+        print("\nPlayer wins!")
+        return PLAYER
+
+    if cards[PLAYER][HAND_SUM] < cards[DEALER][HAND_SUM]:
+        print("\nDealer wins!")
+        return DEALER
+
+    print("\nTie!")
+    return None
+
+def display_wins(wins):
+    print(f'Player wins: {wins[PLAYER]}\tDealer wins: {wins[DEALER]}\n')
+
+def check_match_win(wins):
+    if wins[PLAYER] == FIRST_TO:
+        print('Player wins the match!')
+        wins[PLAYER] = 0
+        wins[DEALER] = 0
+
+    elif wins[DEALER] == FIRST_TO:
+        print('Dealer wins the match!')
+        wins[PLAYER] = 0
+        wins[DEALER] = 0
+
+def main():
+    wins = {PLAYER : 0,
+            DEALER : 0,
+            None : 0,
+    }
+
+    while True:
+        winner = play()
+
+        wins[winner] += 1
+        display_wins(wins)
+        check_match_win(wins)
+
+        response = yes_or_no("Play again? [y/n]: ")
+        if response[0] == 'n':
+            break
+    
+    print("Goodbye!")
+
+if __name__ == "__main__":
+    main()
---
+++ py110/py119_prep/auto_palindrome.py
@@ -0,0 +1,276 @@
+"""
+P:
+    - Make a smart function that creates only numerical palindromes without having to test if its a palindrome, up to a defined number (limit)
+    - Input:
+        - Integer (limit)
+    - Output:
+        - List of integers (list of palindromes)
+    - Rules:
+        - No checking if is a palindrome
+        - At least 2 digits
+E:
+    - 11 , 22, 33, 44, 55, ... 99
+    - 101, 111, 121, 131, 141, 151 ... 191
+    - 202, 212, 222, 232 ... 292
+    - ... 989, 999
+    - 1001, 1111, 1221, 1331, ... 1991
+    - 2002, 2112, 2222, ... 2992
+    - ... 9009, ... 9889, 9999
+    - 10001, 10101, 10201, 10301, ... 10901
+    - 11011, 11111, 11211, 11311
+D:
+    - 1 counter for 2 digits
+    - 2 counters for 3 or 4 digits
+    - 3 counters for 5 or 6 digits
+
+    - Input: Integer (limit)
+    - Output: List of integers (list of palindromes)
+A:
+    - Outer counter, inner counter, outer counter   1|0|1
+    - outer, inner, inner, outer                   10|01
+    - outerr, outer, inner, outer, outerr          21|0|12
+    -                                             210|012
+    -                                             321|0|123
+    -                                            3210|0123
+"""
+import pdb
+from os import system
+
+DIGIT_LIMIT = 2
+LAST_NUM = 10**DIGIT_LIMIT
+
+def palindrome(result):
+    for num in range(LAST_NUM):
+        if num == 0:
+            continue
+        num = str(num)
+        num_string = num + num[::-1]
+        result.append(num_string)
+        if all_nines(num_string):
+            special(result, int(num))
+
+def all_nines(num_string):
+    for num in num_string:
+        if num != '9':
+            return False
+    return True
+
+def special(result, size):
+    for num in range(10**(len(str(size)) - 1), size + 1):
+        if num == 0:
+            continue
+        num = str(num)
+        for add in range(10):
+            add = str(add)
+            num_string = num + add + num[::-1]
+            result.append(num_string)
+
+def main():
+    result = []
+    checks = ['99', '999', '9999', '99999', '999999', '9999999', '99999999' '999999999']
+    palindrome(result)
+    result.append("Done")
+        
+    system("clear")
+    print(result)
+    for idx, n in enumerate(result):
+        for check in checks:
+            if n == check:
+                print(idx, n)
+                print(idx + 1, result[idx + 1])
+
+if __name__ == "__main__":
+    main()
+
+# LOOP_LIMIT = 2
+# # include_first_counter = False
+# include_first_counter = True
+
+# def loop(result, num_string, num_of_loops):
+#     global include_first_counter
+#     # if len(result) > 2000:
+#     #     return
+        
+#     if num_of_loops < LOOP_LIMIT:
+#         num_of_loops += 1
+
+#         for num in range(100):
+#             if num == 0:
+#                 num_string += '00'
+#             elif num < 10:
+#                 num_string = num_string[:-1] + str(num)
+#             else:
+#                 num_string = num_string[:-2] + str(num)
+            
+#             loop(result, num_string, num_of_loops)
+
+#     else:
+#         # switch = all_nines(num_string)
+#         num_string = num_string.lstrip('0')
+#         if not num_string:
+#             for num in range(1, 10):
+#                 result.append(str(num) * 2)
+#             temp_string = ""
+        
+#         elif include_first_counter:
+#             for num in range(10):
+#                 num = str(num)
+#                 temp_string = num_string
+#                 temp_string = temp_string + num + temp_string[::-1]
+#                 result.append(temp_string)
+#                 if all_nines(num_string):
+#                     include_first_counter = flip(include_first_counter)
+#         else:
+#             temp_string = num_string
+#             temp_string = temp_string + temp_string[::-1]
+#             result.append(temp_string)
+#             if all_nines(num_string):
+#                 include_first_counter = flip(include_first_counter)
+#                 loop(result, num_string, num_of_loops)
+
+        
+
+# def all_nines(num_string):
+#     # pdb.set_trace()
+#     if not num_string:
+#         return False
+#     for num in num_string:
+#         if num != '9':
+#             return False
+#     return True
+
+# def flip(include_first_counter):
+#     if include_first_counter:
+#         return False
+#     return True
+
+# checks = ['99', '999', '9999', '99999', '999999', '9999999', '99999999' '999999999']
+# result = []
+# num_string = ""
+# loop(result, num_string, 0)
+# system("clear")
+# # print(result[:500])
+# for idx, n in enumerate(result):
+#     for check in checks:
+#         if n == check:
+#             print(idx, n)
+#             print(idx + 1, result[idx + 1])
+
+
+# def loop(result, num_string, num_of_loops):
+#     global include_first_counter
+#     if len(result) > 200:
+#         return
+        
+#     if num_of_loops != LOOP_LIMIT:
+#         num_of_loops += 1
+
+#         for num in range(100):
+#             num = str(num)
+
+#             if num == '0':
+#                 num_string += num
+#             else:
+#                 num_string = num_string[:-1] + num
+#             loop(result, num_string, num_of_loops)
+
+#     else:
+#         num_string = num_string.lstrip('0')
+#         if not num_string:
+#             return
+
+#         if include_first_counter:
+#             for num in range(10):
+#                 num = str(num)
+#                 temp_string = num_string
+#                 temp_string = temp_string + num + temp_string[::-1]
+#                 result.append(temp_string)
+#         else:
+#             temp_string = num_string
+#             temp_string = temp_string + temp_string[::-1]
+#             result.append(temp_string)
+
+#         if all_nines(num_string):
+#             include_first_counter = flip(include_first_counter)
+
+# def all_nines(num_string):
+#     if not num_string:
+#         return False
+#     for num in num_string:
+#         if num != '9':
+#             return False
+#     return True
+
+# def flip(include_first_counter):
+#     if include_first_counter:
+#         return False
+#     return True
+
+# def last_call(num_string, single):
+    
+
+# do_once = [True for _ in range(LOOP_LIMIT)]
+
+# result = [""]
+# loop(result, "", 0)
+# system("clear")
+# for res in result:
+#     print(res)
+# print(result)
+
+# SINGLE = 1
+# DOUBLE = 0
+
+# def palindrome(limit):
+#     result = []
+#     num = ""
+#     single = True
+#     for three in range(10):
+#         three = str(three)
+#         for two in range(10):
+#             two = str(two)
+#             for one in range(10):
+#                 one = str(one)
+
+#                 if single:
+#                     num = three + two + one + two + three
+#                 else:
+#                     num = three + two + one + one + two + three
+
+#                 num = num.strip('0')
+#                 if len(num) < 2:
+#                     continue
+#                 else:
+#                     result.append(num)
+#                 if len(result) == limit:
+#                     return result
+#     return result
+# print(palindrome(5020))
+
+
+    # for ten in range(10):
+    #     ten = str(ten)
+    #     for nine in range(10):
+    #         nine = str(nine)
+    #         for eight in range(10):
+    #             eight = str(eight)
+    #             for seven in range(10):
+    #                 seven = str(seven)
+    #                 for six in range(10):
+    #                     six = str(six)
+    #                     for five in range(10):
+    #                         five = str(five)
+    #                         for four in range(10):
+    #                             four = str(four)
+    #                             for three in range(10):
+    #                                 three = str(three)
+    #                                 for two in range(10):
+    #                                     two = str(two)
+    #                                     for one in range(10):
+    #                                         one = str(one)
+    #                                         # num = ten + nine + eight + seven + six + five + four + three + two + one + one + two + three + four + five + six + seven + eight + nine + ten
+    #                                         num = ten + nine + eight + seven + six + five + four + three + two + one + two + three + four + five + six + seven + eight + nine + ten
+    #                                         result.append(num.strip('0'))
+    #                                         if len(result) == limit:
+    #                                             return result[-5::]
+
